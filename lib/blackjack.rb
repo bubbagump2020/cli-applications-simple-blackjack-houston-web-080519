59ba7cee0173b74519c9356dceb0650766567d5f
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 16:18:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,6 +40,7 @@ def hit?(card_total)
     card_total += deal_card
   else
     invalid_command
+    prompt_user
   end
   card_total
 end    
@@ -47,7 +48,7 @@ end
 
 def invalid_command
   puts "Please enter a valid command"
-  prompt_user
+  
 end
 
 #####################################################
