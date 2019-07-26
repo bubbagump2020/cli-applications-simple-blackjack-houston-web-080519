853a35ad1460f9e8769c8762f5577c9c30db1d6c
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 15:51:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,12 @@ end
 
 def hit?
   prompt_user
-  get_user_input
+  user_input = get_user_input
+  if user_input = "h"
+    deal_card
+  else
+    display_card_total
+    
 end
 
 def invalid_command
